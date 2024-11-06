6e540683a82e37462a6bc37dec3e6ebce33ba5c8
IsoldaLulu <139595746+IsoldaLulu@users.noreply.github.com>
2024-11-05 22:15:07 -0700

    Update jsboda.js
    cambio2

--- jsboda.js
+++ jsboda.js
@@ -1,17 +1,13 @@
-// Mostrar el modal cuando la página cargue
-window.onload = function() {
-    // Mostrar el modal al cargar la página
-    document.getElementById("openModal").click();
-};
-
 // Función para cerrar el modal y activar la música
 document.getElementById("playMusicBtn").addEventListener("click", function() {
     // Cerrar el modal usando Fancybox
     $.fancybox.close();
 
     // Activar la música de Elfsight
-    var iframe = document.querySelector('.elfsight-app-05c92374-1475-4acb-b7e2-dd7365d8aadf iframe');
-    if (iframe) {
-        iframe.contentWindow.postMessage('{"event":"command","func":"playVideo","args":""}', '*');
-    }
+    setTimeout(function() {
+        var iframe = document.querySelector('.elfsight-app-05c92374-1475-4acb-b7e2-dd7365d8aadf iframe');
+        if (iframe) {
+            iframe.contentWindow.postMessage('{"event":"command","func":"playVideo","args":""}', '*');
+        }
+    }, 500);  // Retardo de 500 ms antes de enviar el mensaje
 });
